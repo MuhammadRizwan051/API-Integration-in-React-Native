e5d7f513b9d7a96605f927e62cc7013dd38bbef3
MuhammadRizwan051 <memonrizwan051@gmail.com>
2022-12-05 17:36:49 +0500

    CHangings

--- config/appNavigation.js
+++ config/appNavigation.js
@@ -80,46 +80,46 @@ const Tab = createBottomTabNavigator();
 const TabNavigator = () => (
   <Tab.Navigator tabBarOptions={{
     showLabel: false,
-    style: {
-      position: 'absolute',
-      bottom: 25,
-      left: 20,
-      right: 20,
-      height: 100,
-      elevation: 0,
-      borderRadius: 15,
-      backgroundColor: '#ffffff',
-      shadowColor: '#7F5DF0',
-      shadowOffset: {
-        width: 0,
-        height: 10
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.5,
-      elevation: 5,
-    }
+    // style: {
+    //   position: 'absolute',
+    //   bottom: 25,
+    //   left: 20,
+    //   right: 20,
+    // height: 100,
+    // elevation: 0,
+    // borderRadius: 15,
+    // backgroundColor: 'red',
+    // shadowColor: '#7F5DF0',
+    // shadowOffset: {
+    //   width: 0,
+    //   height: 10
+    // },
+    // shadowOpacity: 0.25,
+    // shadowRadius: 3.5,
+    // elevation: 5,
+    // }
   }} screenOptions={{ headerShown: false }}>
     <Tab.Screen name="All" component={StackNavigator} options={{
       tabBarIcon: ({ focused }) => (
         <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-          <Image style={{ width: 25, height: 25, tintColor: focused ? 'royalblue' : 'black' }} source={{ uri: 'https://cdn-icons-png.flaticon.com/512/25/25694.png' }} />
-          <Text style={{ marginTop: 2, fontSize: 12, color: focused ? 'royalblue' : 'black' }}>HOME</Text>
+          <Image style={{ width: 22, height: 22, tintColor: focused ? 'royalblue' : 'black' }} source={{ uri: 'https://cdn-icons-png.flaticon.com/512/25/25694.png' }} />
+          <Text style={{ marginTop: 1, fontSize: 12, color: focused ? 'royalblue' : 'black' }}>HOME</Text>
         </View>
       )
     }} />
     <Tab.Screen name="SignIn" component={SignIn} options={{
       tabBarIcon: ({ focused }) => (
         <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-          <Image style={{ width: 25, height: 25, tintColor: focused ? 'royalblue' : 'black' }} source={{ uri: 'https://cdn-icons-png.flaticon.com/512/181/181549.png' }} />
-          <Text style={{ marginTop: 2, fontSize: 12, color: focused ? 'royalblue' : 'black' }}>SIGNIN</Text>
+          <Image style={{ width: 22, height: 22, tintColor: focused ? 'royalblue' : 'black' }} source={{ uri: 'https://cdn-icons-png.flaticon.com/512/181/181549.png' }} />
+          <Text style={{ marginTop: 1, fontSize: 12, color: focused ? 'royalblue' : 'black' }}>SIGNIN</Text>
         </View>
       )
     }} />
     <Tab.Screen name="SignUp" component={SignUp} options={{
       tabBarIcon: ({ focused }) => (
         <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-          <Image style={{ width: 25, height: 25, tintColor: focused ? 'royalblue' : 'black' }} source={{ uri: 'https://cdn-icons-png.flaticon.com/512/152/152533.png' }} />
-          <Text style={{ marginTop: 2, fontSize: 12, color: focused ? 'royalblue' : 'black' }}>SIGNUP</Text>
+          <Image style={{ width: 22, height: 22, tintColor: focused ? 'royalblue' : 'black' }} source={{ uri: 'https://cdn-icons-png.flaticon.com/512/152/152533.png' }} />
+          <Text style={{ marginTop: 1, fontSize: 12, color: focused ? 'royalblue' : 'black' }}>SIGNUP</Text>
         </View>
       )
     }} />
--- screens/Product.js
+++ screens/Product.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Image, Text, TouchableOpacity, View } from 'react-native'
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import Icon from 'react-native-vector-icons/dist/MaterialIcons';
 
 
@@ -7,16 +7,16 @@ function Product({ navigation, route }) {
     console.log(route.params)
     return (
         <>
-            <View style={{ height: '100%' }}>
+            <ScrollView style={{ height: '100%', marginTop: 10, marginBottom: 10 }}>
                 <Text style={{ marginHorizontal: 10, backgroundColor: '#344D67', fontWeight: 'bold', textAlign: 'center', paddingVertical: 5, color: 'white', fontStyle: 'italic', fontSize: 18, borderBottomRightRadius: 20, borderBottomLeftRadius: 20 }}>{route.params.category.slice(0, 1).toUpperCase() + route.params.category.slice(1)}</Text>
-                <View style={{ paddingHorizontal: 30, paddingVertical: 20, backgroundColor: 'white', height: '40%' }}>
+                <View style={{ paddingHorizontal: 30, paddingVertical: 30, backgroundColor: 'white', height: '40%' }}>
                     <Text style={{ color: 'black', fontSize: 20, fontWeight: 'bold', fontStyle: 'italic', textDecorationLine: 'underline', textAlign: 'center', marginTop: 0 }}>{route.params.title}</Text>
                     <View style={{ alignItems: 'center', justifyContent: 'center', height: '80%' }}>
                         <Image source={{ uri: route.params.image }} style={{ resizeMode: 'contain', width: '100%', height: 150 }} />
                     </View>
 
                 </View>
-                <View style={{ marginHorizontal: 10, paddingHorizontal: 15, paddingVertical: 10, backgroundColor: '#344D67', height: '60%', borderTopRightRadius: 30, borderTopLeftRadius: 30 }}>
+                <View style={{ marginHorizontal: 10, paddingHorizontal: 15, paddingTop: 30, paddingBottom: 50, backgroundColor: '#344D67', borderTopRightRadius: 30, borderTopLeftRadius: 30 }}>
                     <Text style={{ color: 'white', fontSize: 20 }}>$ {route.params.price}</Text>
                     <Text style={{ color: 'white', marginVertical: 15, fontStyle: 'italic', textAlign: 'justify' }}>{route.params.description.slice(0, 1).toUpperCase() + route.params.description.slice(1)}</Text>
                     <Text style={{ color: 'white' }}>Product ID: {route.params.id}</Text>
@@ -25,13 +25,13 @@ function Product({ navigation, route }) {
                         <Text style={{ color: 'white', fontSize: 30 }}>{route.params.rating.rate}</Text>
                         <Icon name="star" size={25} color="white" />
                     </View>
-                    <View style={{ justifyContent: 'flex-end' }}>
-                        <TouchableOpacity style={{ backgroundColor: 'white', paddingVertical: 10, borderRadius: 15, alignItems: 'center', marginTop: '2%' }}>
+                    <View style={{ marginBottom: 50, marginTop:20}}>
+                        <TouchableOpacity style={{ backgroundColor: 'white', paddingVertical: 10, borderRadius: 15, alignItems: 'center' }}>
                             <Text style={{ color: 'black', fontWeight: 'bold', fontSize: 18, fontStyle: 'italic', fontFamily: 'sans-serif' }}>Add to Cart</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
-            </View>
+            </ScrollView>
         </>
     )
 }
