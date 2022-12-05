d6906052d66a7f2464c3b1440687d87e15d8df02
MuhammadRizwan051 <memonrizwan051@gmail.com>
2022-12-05 17:22:30 +0500

    Changes in Bottom Navigation

--- config/appNavigation.js
+++ config/appNavigation.js
@@ -65,41 +65,61 @@ import Product from '../screens/Product';
 import SignUp from '../screens/SignUp';
 import SignIn from '../screens/SignIn';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Image, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/dist/MaterialIcons';
 
 const Stack = createNativeStackNavigator();
 const StackNavigator = () => (
   <Stack.Navigator>
-    <Stack.Screen name='Home' component={Home} options={{ headerShown: false }}  />
+    <Stack.Screen name='Home' component={Home} options={{ headerShown: false }} />
     <Stack.Screen name='Product' component={Product} />
   </Stack.Navigator>
 )
 
 const Tab = createBottomTabNavigator();
 const TabNavigator = () => (
-  <Tab.Navigator screenOptions={{ headerShown: false }}>
-    <Tab.Screen name="All" style={{color:'black'}} component={StackNavigator} options={{
-      tabBarIcon: ({focused}) => (
-        <View>
-          {/* <Icon name="home" size={25} color="royalblue" tintColor: focused ? '#e32f45':'#748c94' /> */}
-          <Image style={{width:25, height:25, tintColor: focused ? '#e32f45':'black'}} source={{uri:'https://cdn-icons-png.flaticon.com/512/25/25694.png'}} />
+  <Tab.Navigator tabBarOptions={{
+    showLabel: false,
+    style: {
+      position: 'absolute',
+      bottom: 25,
+      left: 20,
+      right: 20,
+      height: 100,
+      elevation: 0,
+      borderRadius: 15,
+      backgroundColor: '#ffffff',
+      shadowColor: '#7F5DF0',
+      shadowOffset: {
+        width: 0,
+        height: 10
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.5,
+      elevation: 5,
+    }
+  }} screenOptions={{ headerShown: false }}>
+    <Tab.Screen name="All" component={StackNavigator} options={{
+      tabBarIcon: ({ focused }) => (
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Image style={{ width: 25, height: 25, tintColor: focused ? 'royalblue' : 'black' }} source={{ uri: 'https://cdn-icons-png.flaticon.com/512/25/25694.png' }} />
+          <Text style={{ marginTop: 2, fontSize: 12, color: focused ? 'royalblue' : 'black' }}>HOME</Text>
         </View>
       )
-    }}  />
+    }} />
     <Tab.Screen name="SignIn" component={SignIn} options={{
-      tabBarIcon: ({focused}) => (
-        <View>
-          {/* <Icon name="home" size={25} color="royalblue" tintColor: focused ? '#e32f45':'#748c94' /> */}
-          <Image style={{width:25, height:25, tintColor: focused ? '#e32f45':'black'}} source={{uri:'https://cdn-icons-png.flaticon.com/512/181/181549.png'}} />
+      tabBarIcon: ({ focused }) => (
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Image style={{ width: 25, height: 25, tintColor: focused ? 'royalblue' : 'black' }} source={{ uri: 'https://cdn-icons-png.flaticon.com/512/181/181549.png' }} />
+          <Text style={{ marginTop: 2, fontSize: 12, color: focused ? 'royalblue' : 'black' }}>SIGNIN</Text>
         </View>
       )
     }} />
     <Tab.Screen name="SignUp" component={SignUp} options={{
-      tabBarIcon: ({focused}) => (
-        <View>
-          {/* <Icon name="home" size={25} color="royalblue" tintColor: focused ? '#e32f45':'#748c94' /> */}
-          <Image style={{width:25, height:25, tintColor: focused ? '#e32f45':'black'}} source={{uri:'https://cdn-icons-png.flaticon.com/512/152/152533.png'}} />
+      tabBarIcon: ({ focused }) => (
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Image style={{ width: 25, height: 25, tintColor: focused ? 'royalblue' : 'black' }} source={{ uri: 'https://cdn-icons-png.flaticon.com/512/152/152533.png' }} />
+          <Text style={{ marginTop: 2, fontSize: 12, color: focused ? 'royalblue' : 'black' }}>SIGNUP</Text>
         </View>
       )
     }} />
